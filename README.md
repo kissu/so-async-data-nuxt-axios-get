66e96b33858c157a4488ea2e1e10d154575239f8
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-03-28 20:23:40 +0200

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+Related to: https://stackoverflow.com/questions/66815307/using-axios-in-nuxt-asyncdata-lose-token-on-browser-refresh?noredirect=1#comment118109637_66815307
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>fetched data below</p>
+    <p>{{ fetchedData }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const response = await $axios.$get(
+      'https://jsonplaceholder.typicode.com/todos/1',
+      {
+        headers: {
+          'X-AUTH-TOKEN': '### TOKEN HARDCODED ###',
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    return { fetchedData: response }
+  },
+}
+</script>
